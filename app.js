30482450bc4c93839d602daa75e96365f8f6e335
vaidyayash <yashvaidya1297@gmail.com>
2026-01-25 08:28:05 +0000

    Add EJS layout template, navbar, and basic styling

--- app.js
+++ app.js
@@ -19,11 +19,13 @@ async function main(){
 }
 
 // CRUD OPERATIONS
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
 
 app.get("/", (req, res)=>{
   res.send("App is working");
