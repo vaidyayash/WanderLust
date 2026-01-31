24f9d3cd076091cf463327ec076133f21858c5cd
vaidyayash <yashvaidya1297@gmail.com>
2026-01-31 09:15:37 +0000

    Add error page for improved error handling

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.use((req, res, next)=>{
 
 app.use((err, req, res, next) => {
   let {status=500, message="Something Went Wrong"} = err;
-  res.status(status).send(message);
+  res.status(status).render("listings/error", {err});
 });
 
 app.listen(port, () => {
