51c2f3f08f7748f7aa8e7b421fe6b7c341013696
vaidyayash <yashvaidya1297@gmail.com>
2026-02-14 07:21:17 +0000

    add User model with passport-local-mongoose authentication

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", userSchema);
